440784fb1e7cd6626437fc5ee5fb1985117f813b
Kleyton <kleyton_vccamargo@hotmail.com>
2023-01-09 23:45:57 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+
+        var loop = 5;
+
+        for (int i = 1; i < loop + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+    }
+}
